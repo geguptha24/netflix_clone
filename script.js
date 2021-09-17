7294354b479fa90f814d94d202beb6e2d2c3b177
geguptha24 <75922068+geguptha24@users.noreply.github.com>
2021-09-17 20:08:08 +0530

    i added required files

---
+++ script.js
@@ -0,0 +1,28 @@
+const tabItems = document.querySelectorAll('.tab-item');
+const tabContentItems = document.querySelectorAll(".tab-content-item");
+
+// select tab content item:
+
+function selectItem(e){
+    removeBorder();
+    removeShow();
+    // add border to current tab
+    this.classList.add('tab-border');
+    // grab content item from dob
+    const tabContentItem = document.querySelector(`#${this.id}-content`)
+    // add show class
+    tabContentItem.classList.add('show');
+
+}
+
+
+function removeBorder(){
+    tabItems.forEach(item => item.classList.remove('tab-border'));
+}
+
+function removeShow(){
+    tabContentItems.forEach(item => item.classList.remove('show'));
+}
+
+// listen for tab click
+tabItems.forEach(item => item.addEventListener('click',selectItem));
